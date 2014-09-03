d882544e024e289c7e7cdcfeffdf07fd4d4fdad0
marsbard <martin@ciderpunk.com>
2014-09-03 14:40:11 +0200

    reverting service redefinition

--- modules/alfresco-common/manifests/init.pp
+++ modules/alfresco-common/manifests/init.pp
@@ -5,15 +5,23 @@ class alfresco-common{
 	  ensure => present,
 	  require => Exec["apt-get update"],
 	}
+
+
+	# TODO share.war is referenced here but it doesn't want to be if we are installing
+	# share-war but not alfresco-war. Need to work out how to split them out. Note that
+	# it is not possible to reference the tomcat7 service in both alfresco-war and
+	# share-war classes in the case that they are both being loaded (i.e. allinone)
 	service { "tomcat7":
 	  ensure  => "running",
 	  require => [
 		Package["tomcat7"], 
-		File["/var/lib/tomcat7/webapps/alfresco.war"],
 	  	File["/var/lib/tomcat7/webapps/share.war"],
+		File["/var/lib/tomcat7/webapps/alfresco.war"],
 		File["/var/lib/tomcat7/shared/classes/alfresco-global-properties"],
 	  ],
 	}
+
+
 
 	# http://stackoverflow.com/a/18846683
 	# Using "creates" means that this exec is only run if this file does not exist 
