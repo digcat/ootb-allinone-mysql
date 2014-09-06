a5ebc83f8354f02130b81328c29f0d4cae1e80ab
marsbard <martin@ciderpunk.com>
2014-09-06 09:52:32 +0200

    three addons

--- modules/addons/manifests/init.pp
+++ modules/addons/manifests/init.pp
@@ -122,92 +122,92 @@ class addons {
 
 
 
-	############ SAMPLE #3 ##########################################
-	# Building jars from github source                              #
-	#                                                               #
-	# Remember to add require entries in Exec["apply-addons"], or   #
-	# you can use 'before => Exec["apply-addons"]' instead.         #
-	#################################################################
-
-
-	$url_datalist_exts = "https://github.com/deas/fme-alfresco-extdl"
-
-	
-	# get the sources from github
-	exec { "clone-datalist-exts": 
-		cwd => "/tmp",
-		path => "/bin:/usr/bin",
-		require => Package["git"],
-		command => "git clone ${url_datalist_exts}",
-		creates => "/tmp/fme-alfresco-extdl/pom.xml",
-	}
-
-	# build the jars
-	exec { "build-datalist-exts":
-		cwd => "/tmp/fme-alfresco-extdl",
-		path => "/bin:/usr/bin",
-		require => [ 
-			Exec["clone-datalist-exts"], 
-			Package["maven"], 
-			Package["default-jdk"], 
-			#File["/tmp/fme-alfresco-extdl/pom.xml"], 
-		], 
-		command => "mvn package",
-		timeout => 0, # this can be a very long running command the first time it runs, let it be		
-	}
-
-
-	# copy the jars into place
-
-	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/alfresco/WEB-INF"],
-	}
-	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/alfresco"],
-	}
-	file { "/var/lib/tomcat7/webapps/alfresco":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/"],
-	}
-	file { "/var/lib/tomcat7/webapps/share/WEB-INF/lib":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/share/WEB-INF"],
-	}
-	file { "/var/lib/tomcat7/webapps/share/WEB-INF":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/share"],
-	}
-	file { "/var/lib/tomcat7/webapps/share":
-		ensure => directory,
-		require => File["/var/lib/tomcat7/webapps/"],
-	}
-	file { "/var/lib/tomcat7/webapps":
-		ensure => directory,
-		require => File["/var/lib/tomcat7"],
-	}
-
-	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib/fme-alfresco-extdl-repo-1.2.jar":
-		source => "/tmp/fme-alfresco-extdl/fme-alfresco-extdl-repo/target/fme-alfresco-extdl-repo-1.2.jar",
-		ensure => present,
-		
-		# it doesn't actually need to be before apply-addons, not like the amps, but it will get it
-		# in place without a cross-dependency
-		before => Exec["apply-addons"],
-
-		require => [ File["/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib"], Exec["build-datalist-exts"], ],
-	}
-	file { "/var/lib/tomcat7/webapps/share/WEB-INF/lib/fme-alfresco-extdl-share-1.2.jar":
-		source => "/tmp/fme-alfresco-extdl/fme-alfresco-extdl-share/target/fme-alfresco-extdl-share-1.2.jar",
-		ensure => present,
-		# it doesn't actually need to be before apply-addons, not like the amps, but it will get it
-		# in place without a cross-dependency
-		before => Exec["apply-addons"],
-		require => [ File["/var/lib/tomcat7/webapps/share/WEB-INF/lib"], Exec["build-datalist-exts"], ],
-	}
-
-	############ SAMPLE #3 END ######################################
+#	############ SAMPLE #3 ##########################################
+#	# Building jars from github source                              #
+#	#                                                               #
+#	# Remember to add require entries in Exec["apply-addons"], or   #
+#	# you can use 'before => Exec["apply-addons"]' instead.         #
+#	#################################################################
+#
+#
+#	$url_datalist_exts = "https://github.com/deas/fme-alfresco-extdl"
+#
+#	
+#	# get the sources from github
+#	exec { "clone-datalist-exts": 
+#		cwd => "/tmp",
+#		path => "/bin:/usr/bin",
+#		require => Package["git"],
+#		command => "git clone ${url_datalist_exts}",
+#		creates => "/tmp/fme-alfresco-extdl/pom.xml",
+#	}
+#
+#	# build the jars
+#	exec { "build-datalist-exts":
+#		cwd => "/tmp/fme-alfresco-extdl",
+#		path => "/bin:/usr/bin",
+#		require => [ 
+#			Exec["clone-datalist-exts"], 
+#			Package["maven"], 
+#			Package["default-jdk"], 
+#			#File["/tmp/fme-alfresco-extdl/pom.xml"], 
+#		], 
+#		command => "mvn package",
+#		timeout => 0, # this can be a very long running command the first time it runs, let it be		
+#	}
+#
+#
+#	# copy the jars into place
+#
+#	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/alfresco/WEB-INF"],
+#	}
+#	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/alfresco"],
+#	}
+#	file { "/var/lib/tomcat7/webapps/alfresco":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/"],
+#	}
+#	file { "/var/lib/tomcat7/webapps/share/WEB-INF/lib":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/share/WEB-INF"],
+#	}
+#	file { "/var/lib/tomcat7/webapps/share/WEB-INF":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/share"],
+#	}
+#	file { "/var/lib/tomcat7/webapps/share":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7/webapps/"],
+#	}
+#	file { "/var/lib/tomcat7/webapps":
+#		ensure => directory,
+#		require => File["/var/lib/tomcat7"],
+#	}
+#
+#	file { "/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib/fme-alfresco-extdl-repo-1.2.jar":
+#		source => "/tmp/fme-alfresco-extdl/fme-alfresco-extdl-repo/target/fme-alfresco-extdl-repo-1.2.jar",
+#		ensure => present,
+#		
+#		# it doesn't actually need to be before apply-addons, not like the amps, but it will get it
+#		# in place without a cross-dependency
+#		before => Exec["apply-addons"],
+#
+#		require => [ File["/var/lib/tomcat7/webapps/alfresco/WEB-INF/lib"], Exec["build-datalist-exts"], ],
+#	}
+#	file { "/var/lib/tomcat7/webapps/share/WEB-INF/lib/fme-alfresco-extdl-share-1.2.jar":
+#		source => "/tmp/fme-alfresco-extdl/fme-alfresco-extdl-share/target/fme-alfresco-extdl-share-1.2.jar",
+#		ensure => present,
+#		# it doesn't actually need to be before apply-addons, not like the amps, but it will get it
+#		# in place without a cross-dependency
+#		before => Exec["apply-addons"],
+#		require => [ File["/var/lib/tomcat7/webapps/share/WEB-INF/lib"], Exec["build-datalist-exts"], ],
+#	}
+#
+#	############ SAMPLE #3 END ######################################
 
 
 
